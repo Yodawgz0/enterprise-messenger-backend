c2b6992514b64e8944b227e192d46ec8533b823e
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-28 11:53:19 -0400

    adding socket to server

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { createClient } from "redis";
 import { configDotenv } from "dotenv";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
 
 configDotenv();
 
@@ -9,12 +13,19 @@ const uri: string = process.env["DB_URL"]!;
 const password: string = process.env["PASSWORD"]!;
 const port: string = process.env["PORT"]!;
 
+const server = http.createServer(app);
+const io = new Server(server);
+
 const client = createClient({
   password: password,
   socket: {
     host: uri,
     port: parseInt(port),
   },
+});
+
+io.on("connection", (_socket) => {
+  console.log("a user connected");
 });
 
 getData.get("/api", async (_req: Request, res: Response) => {
