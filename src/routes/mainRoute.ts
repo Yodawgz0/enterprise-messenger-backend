09b7376f945897f863831ea12b02c893d1fb9b0b
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-16 17:53:28 -0400

    client correction of connections

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,32 +1,12 @@
 import express, { Request, Response } from "express";
-import { createClient } from "redis";
-import { configDotenv } from "dotenv";
 
-import { user } from "../models/userModel";
-import { registerUser } from "../services/credUser";
-
-configDotenv();
+import { user } from "../models/userModel.ts";
+import { loginUser, registerUser } from "../services/credUser.ts";
 
 const getData = express.Router();
 getData.use(express.json());
-const uri: string = process.env["DB_URL"]!;
-const password: string = process.env["PASSWORD"]!;
-const port: string = process.env["PORT"]!;
-
-const client = createClient({
-  password: password,
-  socket: {
-    host: uri,
-    port: parseInt(port),
-  },
-});
 
 getData.post("/signup", async (_req: Request, res: Response) => {
-  try {
-    await client.connect();
-  } catch (err) {
-    console.log("Client is already connected");
-  }
   if (_req.body.userDetails.username && _req.body.userDetails.password) {
     const userInfo: user = {
       username: _req.body.userDetails.username,
@@ -50,4 +30,25 @@ getData.post("/signup", async (_req: Request, res: Response) => {
   }
 });
 
+getData.get("/login", async (_req: Request, res: Response) => {
+  if (_req.body.userDetails.username && _req.body.userDetails.password) {
+    const userInfo: user = {
+      username: _req.body.userDetails.username,
+      password: _req.body.userDetails.password,
+    };
+    const result: boolean | string = await loginUser(userInfo);
+
+    if (typeof result === "boolean") {
+      if (result === true) {
+        res.status(200).json({ message: "Login Successful!" });
+      } else {
+        res.status(409).json({ error: "User Doesn't Exist! Please Register" });
+      }
+    } else {
+      res.status(500).json({ message: result });
+    }
+  } else {
+    res.status(400).json({ message: "Username and Password required" });
+  }
+});
 export { getData };
--- src/services/credUser.ts
+++ src/services/credUser.ts
@@ -1,9 +1,12 @@
 import { createClient } from "redis";
 import { user } from "../models/userModel";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const uri: string = process.env["DB_URL"]!;
 const password: string = process.env["PASSWORD"]!;
 const port: string = process.env["PORT"]!;
+console.log(port, parseInt(port));
 const client = createClient({
   password: password,
   socket: {
@@ -15,6 +18,12 @@ const client = createClient({
 export const registerUser = async (
   userInfo: user
 ): Promise<string | boolean> => {
+  try {
+    await client.connect();
+  } catch (err) {
+    console.log(err);
+    console.log("Client is already connected");
+  }
   await client
     .get("allUsers")
     .then((result) => {
@@ -33,6 +42,36 @@ export const registerUser = async (
           ];
 
           client.set("allUsers", JSON.stringify(userDataSet));
+          return true;
+        }
+        return false;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return "Network Error!";
+    });
+  return "Something Went Wrong!";
+};
+
+export const loginUser = async (userInfo: user): Promise<string | boolean> => {
+  try {
+    await client.connect();
+  } catch (err) {
+    console.log("Client is already connected");
+  }
+  await client
+    .get("allUsers")
+    .then((result) => {
+      console.log(result);
+      if (result === null) {
+        return false;
+      } else {
+        let userDataSet: user[] = JSON.parse(result);
+        console.log(userDataSet);
+        if (
+          userDataSet.filter((e) => e.username === userInfo.username).length
+        ) {
           return true;
         }
         return false;
