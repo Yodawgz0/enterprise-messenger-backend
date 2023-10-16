ac5e13179e2c3e2da7eb2b667f70cf0cf8744ac3
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-16 17:18:36 -0400

    adding returns for error handling

--- src/services/credUser.ts
+++ src/services/credUser.ts
@@ -12,25 +12,35 @@ const client = createClient({
   },
 });
 
-export const registerUser = async (userInfo: user) => {
-  await client.get("allUsers").then((result) => {
-    if (result === null) {
-      client.set("allUsers", JSON.stringify([{ ...userInfo, id: 1 }]));
-      return true;
-    } else {
-      let userDataSet: user[] = JSON.parse(result);
-
-      if (userDataSet.filter((e) => e.username !== userInfo.username).length) {
-        userDataSet = [
-          ...userDataSet,
-          { ...userInfo, id: userDataSet.length + 1 },
-        ];
-
-        client.set("allUsers", JSON.stringify(userDataSet));
+export const registerUser = async (
+  userInfo: user
+): Promise<string | boolean> => {
+  await client
+    .get("allUsers")
+    .then((result) => {
+      if (result === null) {
+        client.set("allUsers", JSON.stringify([{ ...userInfo, id: 1 }]));
         return true;
       } else {
+        let userDataSet: user[] = JSON.parse(result);
+
+        if (
+          userDataSet.filter((e) => e.username !== userInfo.username).length
+        ) {
+          userDataSet = [
+            ...userDataSet,
+            { ...userInfo, id: userDataSet.length + 1 },
+          ];
+
+          client.set("allUsers", JSON.stringify(userDataSet));
+          return true;
+        }
         return false;
       }
-    }
-  });
+    })
+    .catch((error) => {
+      console.log(error);
+      return "Network Error!";
+    });
+  return "Something Went Wrong!";
 };
